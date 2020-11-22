d9a110e97dc559ca26cd854e4a7b937e982897ed
Chiorean Andrei <69081190+CAndrei1601@users.noreply.github.com>
2020-11-22 23:40:23 +0200

    Add files via upload

---
+++ build/precache-manifest.4aa067671f017814200bc1e938d35efa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea6a224e0b01c61a0ffafe2c816c4269",
+    "url": "/FaceDetection/index.html"
+  },
+  {
+    "revision": "8fca4f215754b1b05e6e",
+    "url": "/FaceDetection/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "e1dcc4ab8dc392528f51",
+    "url": "/FaceDetection/static/css/main.ebcf5acd.chunk.css"
+  },
+  {
+    "revision": "8fca4f215754b1b05e6e",
+    "url": "/FaceDetection/static/js/2.64a6c0dd.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/FaceDetection/static/js/2.64a6c0dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1dcc4ab8dc392528f51",
+    "url": "/FaceDetection/static/js/main.30391f64.chunk.js"
+  },
+  {
+    "revision": "42cd5ec89cd79d8da83a",
+    "url": "/FaceDetection/static/js/runtime-main.db6a2446.js"
+  },
+  {
+    "revision": "9fb689a3c6e019cf9474fde9ff7710a6",
+    "url": "/FaceDetection/static/media/logo.9fb689a3.png"
+  }
+]);
